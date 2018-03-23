860d7df48ba68ac920eb26cbd283da84be39ced2
Youlich <jutatibouet@yahoo.fr>
2018-03-23 16:50:39 +0100

    modif view publication

--- view/frontend/Publications.php
+++ view/frontend/Publications.php
@@ -13,18 +13,26 @@
 <body class="top">
 
 <br/><br/>
+<!-- La page complète -->
 <div class="container">
 
+    <!-- section livre -->
     <div class="form-group">
+
+        <form action="index.php?action=publier" method="post">
             <div>
-                <label>Livre</label>
+                <label>Sélectionnez votre livre</label>
                 <select name="book" id="book" size="1">
                     <?php foreach ($books as $book) { ?>
-                    <option value="<?php echo $book->getTitle();  ?>"><?php echo $book->getTitle();  ?></option> <?php } ?>
+                    <option value="book"><?php echo $book->getTitle();  ?></option> <?php } ?>
+
                 </select>
-                <!-- cette sélection doit donner un bookID pour le chapitre -->
+            </div>
+            <div>
+                <input type="submit" class="button" value="Sélectionner" />
             </div>
             <br/>
+        </form>
             <div>  <a href="index.php?action=publier&amp;publierlivre=1">
                     <input type="button" class="bouton" name= "button" value="Publier un nouveau livre"></a></div>
             <?php if (!empty($_GET['publierlivre'])): ?>
@@ -41,7 +49,7 @@
                     </tr>
                     <tr>
                         <td >
-                            <input class="btn btn-success btn-md" type="submit" id="submitaddbook" name="submitaddbook" value="Ajouter" />
+                            <input class="btn btn-success btn-md" type="submit" id="submit" name="submit" value="Ajouter" />
                         </td>
                     </tr>
                 </table>
@@ -50,25 +58,42 @@
     <br/>
    </div>
     <br/>
+
+    <!-- Fin section livre -->
+
+    <!-- Section chapitre -->
+
     <div class="form-group">
+
+        <form action="index.php?action=publier" method="post">
             <div>
-                <label>Chapitre</label>
-                <select name="chapitre" id="chapitre" size="1">
+                <label>Sélectionnez votre chapitre</label>
+                <select  id="chapter" size="1" >
                     <?php foreach ($chapters as $chapter) { ?>
-                    <option value="<?php echo $chapter->getTitle(); ?>"><?php echo $chapter->getTitle(); ?></option> <?php } ?>
+                    <option name="chapterSelect" value="$selectChapter"><?php echo $chapter->getTitle(); ?></option> <?php } ?>
+             <div>
+                <input type="submit" name="chapter" class="button" value="Sélectionner" />
+            </div>
                 </select>
             </div>
+
         <br/>
-                <div> <a href="index.php?action=publier&amp;publierchapitre=1">
+        </form>
+
+                <div> <a href="index.php?action=publier&amp;publierchapitre=1 ">
                         <input type="button" class="bouton" name= "button" value="Publier un nouveau chapitre"></a></div>
-                <?php if (!empty($_GET['publierchapitre'])): ?>
+                        <div> <a href="index.php?action=publier&amp;modifierchapitre=1">
+                                <input type="button" class="bouton" name="button" value="Modifier le chapitre"></a></div>
+                        <div><a href="index.php?action=publier&amp;supprimerchapitre=1">
+                                <input type="button" class="bouton" name="button" value="Supprimer le chapitre"></a></div>
+                <?php if (!empty($_GET['publierchapitre'])){ ?>
                 <form action="index.php?action=addchapter" method="post" enctype="multipart/form-data" >
                     <table>
                         <tr>
                             <td>
                                 <br/><br/>
                                 <input type="hidden" value="" <!-- ici le bookId -->
-                                <label for="titre">Nouveau titre de chapitre : </label>
+                                <label for="titre">Titre du chapitre : </label>
                             </td>
                             <td><br/><br/>
                             <input type="text" name="titrechapitre" id="titrechapitre" style="width: 200%;" placeholder="Chapitre x : titre"/>
@@ -99,22 +124,50 @@
                         </tr>
                     </table>
                 </form>
-                <?php endif; ?>
+               <?php } elseif (!empty($_GET['modifierchapitre'])){ ?>
+        <form action="index.php?action=modifchapter" method="post" enctype="multipart/form-data" >
+            <table>
+                <tr>
+                    <td>
+                        <br/><br/>
+                        <input type="hidden" value="<?php $chapter->getbookId(); ?>"
+                        <label for="titre">Nouveau titre de chapitre : </label>
+                    </td>
+                    <br/><br/>
+                    <td>
+
+                        <input type="text" name="titrechapitre" id="titrechapitre" style="width: 200%;" placeholder=""/>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Contenu</label>
+                    </td>
+                    <td>
+                        <textarea class="content" id="contenu" name="contenu" rows="15" placeholder="<?php echo $chapter->getContent(); ?>" ></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Télécharger une image<br/> (tous formats | max. 1 Mo) :</label>
+                    </td>
+                    <td>
+                        <input type="file" name="image" id="image"/>
+                        <input type="hidden" name="MAX_FILE_SIZE" value="12345" placeholder="<?php echo $chapter->getImage(); ?>" />
+
+                    </td>
+                </tr>
+                <tr>
+                    <td >
+                        <input class="btn btn-success btn-md" type="submit" id="submit" name="submit" value="Publier" />
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+               <?php } ?>
         <br/>
-    </div>
-    <br/>
-    <div class="form-group">
-
-
-
-
-    </div>
-    <br>
-
-    <div class="form-group" align="center">
-
-        <button type="submit" class="btn btn-primary btn-xl" id="Button">Modifier</button>
-        <button type="submit" class="btn btn-primary btn-xl" id="Button">Supprimer</button>
     </div>
 
 
