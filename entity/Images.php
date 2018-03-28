9bfba821f2a2a45b9262429dda394dd62253d2c8
Youlich <jutatibouet@yahoo.fr>
2018-03-28 17:37:50 +0200

    ajout table SQL image et class image, variable image qui bloque l'ajout, supp et modif de chapitre

---
+++ entity/Images.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jutat
+ * Date: 28/03/2018
+ * Time: 09:28
+ */
+
+namespace entity;
+
+
+class Images
+{
+
+}
---
+++ model/ImagesManager.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jutat
+ * Date: 28/03/2018
+ * Time: 09:30
+ */
