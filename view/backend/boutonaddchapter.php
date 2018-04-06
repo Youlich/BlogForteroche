b3054cc81c07e7479fd03a1ac85ced5278ad71a1
Youlich <jutatibouet@yahoo.fr>
2018-04-06 16:15:13 +0200

    problème image

--- view/backend/boutonaddchapter.php
+++ view/backend/boutonaddchapter.php
@@ -18,18 +18,18 @@
 
     <!-- Bouton ajout livre -->
     <a href="index.php?action=boutonaddbook">
-            <input type="button" class="btn btn-secondary btn-lg btn-block" name= "button" value="Publier un nouveau livre"></a>
+        <input type="button" class="btn btn-secondary btn-lg btn-block" name= "button" value="Publier un nouveau livre"></a>
     <br/><br/><br/>
 
     <!-- Bouton ajout chapitre -->
     <a href="index.php?action=boutonaddchapter">
-            <input type="button" style="text-decoration: none;" class="btn btn-secondary btn-lg btn-block" name= "button" value="Publier un nouveau chapitre"></a>
+        <input type="button" style="text-decoration: none;" class="btn btn-secondary btn-lg btn-block" name= "button" value="Publier un nouveau chapitre"></a>
     <br/>
 
     <form action="" method="post">
         <div class= "input-group" >
             <select name="bookSelect" class= "custom-select" id= "inputGroupSelect04" >
-                <option selected > Choisissez votre livre </option>
+                <option selected > <?php echo $selectedbook ?> </option>
                 <?php foreach ($books as $book) { ?>
                     <option value="<?php echo $book->getId(); ?>"><?php echo $book->getTitle();  ?></option> <?php } ?>
             </select>
@@ -44,15 +44,12 @@
         <!-- Saisie des informations : titre, contenu et téléchargement image-->
 
         <form action="index.php?action=addchapter" method="post" enctype="multipart/form-data" >
+
             <div class= "input-group">
-                <input type="hidden" name="bookSelect" value="<?php echo $_POST['bookSelect'] ?>"
-                <label for="titrelivre">Titre du livre : </label>
-                <input type="text" name="titrelivre" id="titrelivre"  style="width: 200%;" value="<?php echo $bookSelect->getTitle(); ?>"/>
-            </div>
-            <br/><br/>
-            <div class= "input-group">
+                <input type="hidden" name="bookSelect" value="<?php echo $selectedbook ?>">
                 <label for="titrechapitre">Titre du chapitre : </label>
                 <input type="text" name="titrechapitre" id="titrechapitre"  style="width: 200%;" placeholder="Chapitre x : titre"/>
+                <input type="hidden" name="newchapterId" value="<?php echo $newchapterId ?>">
             </div>
             <br/><br/>
             <div class= "input-group">
@@ -103,12 +100,12 @@
 <div align="center" id="endpage"> <a href="index.php?action=administration">Retour page d'administration</a> </div>
 <br/>
 <br/>
-    <!-- Scroll to Top Button -->
-    <div class="scroll-to-top position-fixed ">
-        <a class="js-scroll-trigger d-block text-center text-white rounded" href="#top">
-            <i class="fa fa-chevron-up"></i>
-        </a>
-    </div>
+<!-- Scroll to Top Button -->
+<div class="scroll-to-top position-fixed ">
+    <a class="js-scroll-trigger d-block text-center text-white rounded" href="#top">
+        <i class="fa fa-chevron-up"></i>
+    </a>
+</div>
 </body>
 <?php include('view/frontend/Footer.php');?>
 
