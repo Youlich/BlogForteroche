371cc2ca2b2b227a446cc77c957a252e9b4b6287
Youlich <jutatibouet@yahoo.fr>
2018-04-27 13:27:42 +0200

    rajout d'un if dans index

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-
-
 require_once('services/Autoload.php');
 
 Autoload::register();
 \services\Config::start();
 $routes = \services\Config::getRoutes();
 session_start();
-$container = new \services\Container([]);
-$router = new \router\Router($container, $routes, $_SERVER['REQUEST_URI']);
-$resolve = $router->resolve();
-$caller = $resolve['controller'];
-$controller = $container->$caller();
-$action = $resolve['action'];
-call_user_func_array([$controller, $action], $resolve['params']);
+
+if ($_SERVER['REQUEST_URI'] == (HOST .'index.php')) {
+    require (VIEWFRONT. 'accueil.php');
+} else {
+    $container = new \services\Container([]);
+    $router = new \router\Router($container, $routes, $_SERVER['REQUEST_URI']);
+    $resolve = $router->resolve();
+    $caller = $resolve['controller'];
+    $controller = $container->$caller();
+    $action = $resolve['action'];
+    call_user_func_array([$controller, $action], $resolve['params']);
+}
